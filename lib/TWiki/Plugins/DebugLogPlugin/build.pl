a06d7bde5ef2a2dd0caba2f8e8103150636658c0
WillNorris <WillNorris@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-04-20 03:34:29 +0000

    Item1476: BuildContrib support
    git-svn-id: http://svn.foswiki.org/trunk/DebugLogPlugin@3568 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

---
+++ lib/TWiki/Plugins/DebugLogPlugin/build.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl -w
+#
+# Requires the environment variable FOSWIKI_LIBS (a colon-separated path
+# list) to be set to point at the build system and any required dependencies.
+# Usage: ./build.pl [-n] [-v] [target]
+# where [target] is the optional build target (build, test,
+# install, release, uninstall), test is the default.
+# Two command-line options are supported:
+# -n Don't actually do anything, just print commands
+# -v Be verbose
+#
+
+# Standard preamble
+BEGIN {
+    unshift @INC, split( /:/, $ENV{FOSWIKI_LIBS} );
+}
+
+use Foswiki::Contrib::Build;
+
+# Create the build object
+$build = new Foswiki::Contrib::Build( 'DebugLogPlugin' );
+
+# Build the target on the command line, or the default target
+$build->build($build->{target});
+
